aca19422726904432042be5743c40460fd2f2d7b
Taisuke Maekawa <fygar256@outlook.jp>
2024-09-08 14:00:16 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-# uncomment following line if you use google translators
-#import translators.server as tss
+try:
+    import translators.server as tss
+except:
+    print("[Failed] to import translator API module could not be done.")
+
 import requests
-import speech_recognition as sr
+import json
 import time
 
+try:
+    import speech_recognition as sr
+except:
+    print("[Failed] to import speech_recognition API module could not be done.")
+
+sid=1
+debug='n'
+
 class AudioToText:
-    def __init__(self, audio_file, debug):
+    def __init__(self, audio_file):
         self.audio_file = audio_file
         self.recognizer = sr.Recognizer()
-        self.debug = debug
 
     def convert(self):
         try:
@@ -18,7 +28,7 @@ class AudioToText:
             print(f"[Success] You say: {text}")
             return text
         except Exception as e:
-            if self.debug == "y":
+            if debug == "y":
               print(e)
             return None
             
@@ -49,24 +59,29 @@ class TextTranslator:
             return translated_text
         except:
             return None
-#
+
 
 class TextToSpeech:
     def __init__(self, text):
         self.text = text
 
-    def generate_audio(self, speaker_id=1):
+    def generate_audio(self):
         try:
-            url = f"https://api.tts.quest/v1/voicevox/?text={self.text}&speaker={speaker_id}"
-            response = requests.get(url).json()
+            url = f"https://api.tts.quest/v1/voicevox/?text={self.text}&speaker={sid}"
+            if debug=="y":print(url)
+            while True:
+                response = requests.get(url).json()
+                if debug=="y":print(response)
+                if response["success"] == True:
+                    break
+                i=int(response["retryAfter"])
+                sleep(i+1)
+
+            if debug=="y":print(response)
             if response["success"] == True:
+                if debug=="y":print("response is success")
                 audio_url = response["mp3DownloadUrl"]
-                status_url= response["audioStatusUrl"]
-                while True:
-                    response2 = requests.get(status_url).json()
-                    if response2["isAudioReady"] == True:
-                        break
-                    time.sleep(3)
+                if debug=="y":print("got mp3 download url")
                 return audio_url
             else:
                 return None
@@ -74,19 +89,20 @@ class TextToSpeech:
             return None
 
     def save_audio(self, audio_url, file_name):
-        audio_content = requests.get(audio_url).content
-        if not audio_content:
+        try:
+            if debug=="y":print("pass")
+            audio_content = requests.get(audio_url).content
+            if debug=="y":print("pass")
+            with open(file_name, "wb") as file:
+                file.write(audio_content)
+            return True
+        except:
             return False
-        file=open(file_name, "wb")
-        file.write(audio_content)
-        file.close()
-        return True
 
 
 if __name__ == "__main__":
-
     audio_file = "assets/audio/testing.wav"
-    section = int(input("Select source \n1. Audio\n2. Mic\n3. Text File\n4. Text File(Japanese)\n: "))
+    section = int(input("Select source \n1. Audio\n2. Mic\n3. Text file(Japanese)\n: "))
     debug = input("Enable debug mode? y/n: ")
     
     if section == 1:
@@ -103,40 +119,41 @@ if __name__ == "__main__":
       if not text:
           print("[Failed] Failed to convert audio file to text")
           exit()
-    elif section == 3 or section==4:
-      # read text file
-      fn = input("File name to read: ")
-      f=open(fn,"r")
+    elif section == 3:
+      # plain text
+      f=open(input("Input file name to speak:"),"rt")
       text = f.read()
       f.close()
+      if not text:
+        print("[Failed] failed to read text file")
+        exit()
     else:
       print("Invalid key.")
-      exit()
-    
-    if section==1 or section ==2 or section==3:
+
+    if  section !=3:
         # translate text to Japanese
         text_translator = TextTranslator(text)
         translated_text = text_translator.translate("id", "ja")
         if not translated_text:
             print("[Failed] Failed to translate text")
             exit()
-    elif section==4:
-        translated_text = text
-    
-    print(translated_text)
-    
+    else:
+        translated_text=text
+
     # generate audio file
-    text_to_speech = TextToSpeech(translated_text)
+    if debug=='y':print("1 pass")
+    text_to_speech = TextToSpeech(translated_text.strip())
+    if debug=='y':print("2 pass")
     audio_url = text_to_speech.generate_audio()
+    if debug=='y':print("3 pass")
     if not audio_url:
         print("[Failed] Failed to generate audio")
         exit()
-    
+
     # save audio file
-    file_name = fn+".mp3"
+    file_name = "test.mp3"
+    if debug=="y":print("pass")
     if text_to_speech.save_audio(audio_url, file_name):
         print(f"[Success] Audio file saved as {file_name}")
     else:
-        print("[Failed] Audio file not saved.")
-    exit()
-    
+        print("[Failed] Audio file not saved")
